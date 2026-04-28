2246c9fa360e734b65410b2437cb7a52003f32e3
virzarasydann <virzarasydan@gmail.com>
2026-04-28 17:23:19 +0700

    first commit

---
+++ JurnalModul8_103082400034/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JurnalModul8_103082400034
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
